0c049f6a0225c1c2d5c845128bde608a84adb551
harissheikh8775 <harisbadboi@gmail.com>
2025-10-25 09:41:40 +0530

    life in weeks left challenge done

---
+++ day_8/challenge_1.py
@@ -0,0 +1,20 @@
+# Life in Weeks challenge
+"""
+Create a function called life_in_weeks() using maths and f-Strings that tells us how many weeks we have left, if we live until 90 years old.
+
+It will take your current age as the input and output a message with our time left in this format:
+
+You have x weeks left.
+
+Where x is replaced with the actual calculated number of weeks the input age has left until age 90.
+
+**Warning** The function must be called life_in_weeks for the tests to pass. Also the output must have the same punctuation and spelling as the example. Including the full stop!
+
+Example Input
+
+56
+
+Example Output
+
+You have 1768 weeks left.
+"""
---
+++ day_8/task_1.py
@@ -0,0 +1,11 @@
+# create a function greet()
+def greet():
+    print("Hello Haris , how are you!!!")
+greet()
+
+# Functions that allows for inputs
+def greet_with_name(name):
+    print(f"Hello {name}.")
+    print(f"How do you do {name}.")
+
+greet_with_name("Haris")
