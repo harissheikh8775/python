4893470bfdff8abb1e5c7ccace0a287c40e4da9c
harissheikh8775 <harisbadboi@gmail.com>
2025-11-03 10:45:14 +0530

    .

--- day_11/code.cpp
+++ day_11/code.cpp
@@ -1,7 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << (bool)3 + 2 << endl;
-    cout << (23.5 + 2 + 'A');
+int main()
+{
+    int n = 4;
+    char charru = 'A';
+    // int vallu = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << charru;
+            charru += 1;
+        }
+        cout << endl;
+    }
 }
